f9646491f488a5bfb00323051759af44cbf7bec3
baul <me@paulhldbrndt.com>
2021-03-10 02:16:45 +0100

    Added Exception handling

--- pages/api/example.js
+++ pages/api/example.js
@@ -2,9 +2,15 @@ export default function handler(req, res) {
 
   const { APP_KEY } = process.env;
   const { ACTION_KEY } = req.headers.authorization.split(" ")[1];
-  
-  if (ACTION_KEY === APP_KEY) {
-    // Process the POST request
-    res.status(200).json({ success: 'true' })
+
+  try {
+    if (ACTION_KEY === APP_KEY) {
+      // Process the POST request
+      res.status(200).json({ success: 'true' })
+    } else {
+      res.status(401)
+    }
+  } catch(err) {
+    res.status(500)
   }
 }
